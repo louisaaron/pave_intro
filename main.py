87bc30f7bf51dc8224fda3a19f087e65459ed4f1
Bashar Eid <8ashareid@gmail.com>
2022-01-24 16:17:28 -0500

    Not done

---
+++ main.py
@@ -0,0 +1,46 @@
+import cv2
+
+# double array that stores RGB values of associated pixels from image
+img = cv2.imread("images\lake00.jpeg")
+
+# image height (pixels).. number of rows
+height = len(img)
+
+# image length (pixels).. number of columns
+length = len(img[0])
+
+# loop to find the topmost pixel of the buoy
+for i in range(height): # row
+    for j in range(length): # column
+        B = img[i][j][0]
+        G = img[i][j][1]
+        R = img[i][j][2]
+
+        red = R > G and R > B
+
+        if red:
+            break
+
+    if red:
+        break
+
+# diameter of buoy (pixels)
+diameter = 0
+
+for k in range(i, height):
+    B = img[k][j][0]
+    G = img[k][j][1]
+    R = img[k][j][2]
+
+    red = R > G and R > B
+
+    if red:
+        diameter = diameter + 1
+    else:
+        break
+
+# radius of buoy (pixels)
+radius = int(diameter / 2)
+
+# center as (x, y) coordinates as pixels! Bottom left corner is origin
+center = (j, height - (i + radius))
