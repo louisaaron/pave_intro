25a93807785a1a3635ff5c42b2c2538952a37ac1
Bashar Eid <8ashareid@gmail.com>
2022-01-24 16:23:54 -0500

    Not done

--- main.py
+++ main.py
@@ -24,9 +24,12 @@ for i in range(height): # row
     if red:
         break
 
+# at this point, i is row of topmost pixel, and j is column
+
 # diameter of buoy (pixels)
 diameter = 0
 
+# loop to find diameter
 for k in range(i, height):
     B = img[k][j][0]
     G = img[k][j][1]
@@ -39,8 +42,11 @@ for k in range(i, height):
     else:
         break
 
+# at this point, i is row of topmost pixel, and j is column
+# k is row of bottommost pixel
+
 # radius of buoy (pixels)
 radius = int(diameter / 2)
 
-# center as (x, y) coordinates as pixels! Bottom left corner is origin
+# center as (x, y) coordinates in pixels! Bottom left corner is origin
 center = (j, height - (i + radius))
